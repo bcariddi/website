0bd92565706128f04cb50853d66b2f7f2c8ea4eb
Brian Cariddi <bcariddi@nd.edu>
2021-01-26 17:47:32 -0500

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,75 +1,49 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: 'http://briancariddi.com',
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Brian Cariddi',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: 'Brian Cariddi | Developer',
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: 'CS @ Notre Dame',
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: 'bcariddi',
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: 'https://github.com/bcariddi',
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: 'https://www.linkedin.com/in/bcariddi',
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: 'Hi, I\'m Brian!',
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'Spotify Playlist Lyrics',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'ChromeExtensionKit',
-        description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'Tool to analyze the sentiment of all lyrics in a playlist using Spotify API and Python',
+        link: 'https://github.com/bcariddi',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Booz Allen Hamilton',
+        description: 'Data Science Intern, Summer 2020',
+        link: 'https://github.com/bcariddi',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'Languages',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
-      },
-      {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+          'Python, C, C++',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Git',
       },
     ],
   },
